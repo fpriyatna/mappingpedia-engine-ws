033308cd0f7598eb8bd75797d5f64f63ad9c5b0a
Freddy Priyatna <freddy.priyatna@gmail.com>
2018-06-26 16:00:05 +0200

    update log

--- src/main/java/es/upm/fi/dia/oeg/mappingpedia/MappingPediaController.java
+++ src/main/java/es/upm/fi/dia/oeg/mappingpedia/MappingPediaController.java
@@ -851,7 +851,8 @@ public class MappingPediaController {
         logger.info("dataset_id = " + datasetID);
         logger.info("ckan_package_id = " + ckanPackageId);
         logger.info("ckan_package_name = " + ckanPackageName);
-        logger.info("mapping_language = " + pMappingLanguage1);
+        logger.info("ckan_resource_id = " + ckanResourceId);
+       logger.info("mapping_language = " + pMappingLanguage1);
         logger.info("mappingLanguage = " + pMappingLanguage2);
         try {
             boolean generateManifestFile = MappingPediaUtility.stringToBoolean(pGenerateManifestFile);
